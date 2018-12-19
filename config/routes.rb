0791868f9e09202fe0de8af9bb0fee3c1534dd15
suman saurabh <suman.saurabh@grabonrent.com>
2018-12-19 21:09:23 +0530

    pointing root to slugs#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get '/' => "slugs#index"
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       get '/slugs' => 'slugs#index'
